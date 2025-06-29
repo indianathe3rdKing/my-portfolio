03ac498ffcdd3858b6eaa77af9529c7f23d06b40
indianathe3rd <indianathe3rd@gmail.com>
2025-06-29 20:00:51 +0200

    projects section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import ProjectDetails from "./ProjectDetails";
 
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({
+  title,
+  description,
+  subDescription,
+  href,
+  image,
+  tags,
+  setPreview,
+}) => {
+  const [isHidden, setIsHidden] = useState(false);
+  return (
+    <>
+      <div
+        className="flex-wrap items-center py-10 justify-between space-y-14 sm:flex sm:space-y-0"
+        onMouseEnter={() => setPreview(image)}
+        onMouseLeave={() => setPreview(null)}
+      >
+        <p className="text-2xl">{title}</p>
+        <div className="flex gap-5 mt-2 text-sand">
+          {tags.map((tag) => (
+            <span key={tag.id}>{tag.name}</span>
+          ))}
+        </div>
+        <button
+          onClick={() => setIsHidden(true)}
+          className="flex items-center gap-1 cursor-pointer hover-animation"
+        >
+          Read More
+          <img src="src/public/assets/arrow-right.svg" alt="Arrow" />
+        </button>
+      </div>
+      <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
+      {isHidden && (
+        <ProjectDetails
+          title={title}
+          description={description}
+          subDescription={subDescription}
+          tags={tags}
+          href={href}
+          image={image}
+          closeModal={() => setIsHidden(false)}
+        />
+      )}
+    </>
+  );
 };
 
 export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,36 @@
+import { useMotion } from "@react-three/drei";
 import { myProjects } from "../constants/index";
 import Project from "./Project";
+import { motion, useMotionValue, useSpring } from "motion/react";
+import { useState } from "react";
 
 const Projects = () => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const springX = useSpring(x, { damping: 10, stiffness: 50 });
+  const springY = useSpring(y, { damping: 10, stiffness: 50 });
+  const handleMouseMove = (e) => {
+    x.set(e.clientX + 20);
+    y.set(e.clientY + 20);
+  };
+  const [preview, setPreview] = useState(null);
   return (
-    <section className="relative c-space section-spacing">
+    <section
+      onMouseMove={handleMouseMove}
+      className="relative c-space section-spacing"
+    >
       <h2 className="text-heading">My Selected Projects</h2>
       <div className="bg-gradient-to-r from transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"></div>
       {myProjects.map((project) => (
-        <Project key={project.id} />
+        <Project key={project.id} {...project} setPreview={setPreview} />
       ))}
+      {preview && (
+        <motion.img
+          className="fixed top-0 left-0 rounded-lg object-cover shadow-lg pointer-events-none  h-56  w-80"
+          src={preview}
+          style={{ x: springX, y: springY }}
+        />
+      )}
     </section>
   );
 };
