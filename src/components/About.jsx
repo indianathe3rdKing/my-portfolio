84ea6fd26b2e24f2226f834344121d24e9f54e63
indianathe3rd <indiemenziwa@gmail.com>
2025-12-28 23:59:39 +0200

    Update location description in About component for clarity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -86,7 +86,7 @@ const About = () => {
             <div className="z-10 w-[50%]">
               <p className="headtext">Time Zone</p>
               <p className="subtext">
-                I'm based in Port Elizabeth and open to work worldwide
+                I'm based in Port Elizabeth and open to work countrywide
               </p>
             </div>
             <div className=" absolute bottom-[10%] right-[10%] subtext">
